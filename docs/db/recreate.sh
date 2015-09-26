e75bb858d18fb6547c701e0dc70919ceb0389258
Divakar <divakar.dass@tataatsu.com>
2015-09-27 00:37:52 +0530

    Added Tables in schema

--- docs/db/recreate.sh
+++ docs/db/recreate.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 psql -U postgres -d postgres -f dbUser.sql
-psql -U postgres -d postgres -c "DROP DATABASE paypal;"
-psql -U postgres -d postgres -c "CREATE DATABASE paypal WITH owner=foo ENCODING='utf-8';"
-psql -U postgres -d paypal -c 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";'
+psql -U postgres -d postgres -c "DROP DATABASE flexiride;"
+psql -U postgres -d postgres -c "CREATE DATABASE flexiride WITH owner=foo ENCODING='utf-8';"
+psql -U postgres -d flexiride -c 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";'
 
-psql -U foo -d paypal -f schema.sql
+psql -U foo -d flexiride -f schema.sql
