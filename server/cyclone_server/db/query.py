ce727ad523aec4496464f60e828495ad32d6f12f
bharanidharanm <bharanidharan.m@tataatsu.com>
2015-09-27 13:35:46 +0530

    Small Fix

--- server/cyclone_server/db/query.py
+++ server/cyclone_server/db/query.py
@@ -2,7 +2,7 @@ _INSERT_BOOKED_CAB = \
     'INSERT INTO CabsBooked(driver_name, cab_number,'\
     ' driver_mobile_number, sharing, device_id,'\
     ' estimated_amount, estimated_time VALUES(%s, %s,'\
-    ' %s, %s, %s, %s, %s);'
+    ' %s, %s, %s, %s, %s) RETURNING *;'
 
 _INSERT_INTO_USER_DETAILS = \
     'INSERT INTO UserDetails(booking_id, user_mobile_number,'\
